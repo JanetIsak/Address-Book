42f4c6946471a45b351259559d360306495d5258
Janet Isak <janetisak@yahoo.com>
2018-04-04 16:50:46 +0300

    add js and the address constructor

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,16 @@
 function Contact(first, last) {
   this.firstName = first;
   this.lastName = last;
+  this.addresses = [];
 }
-
+function Address(street, city, state) {
+  this.street = street
+  this.city = city
+  this.city = state
+}
+Contact.prototype.fullName = function () {
+  return this.firstName + "" + this.lastName;
+}
 //user interface logic
 $(document).ready(function() {
   $("form#new-contact").submit(function(event) {
@@ -13,17 +21,18 @@ $(document).ready(function() {
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
 
-    var newContact = $(inputtedFirstName, inputtedLastName);
+    var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstname + "</span></li>");
+    $("ul#contacts").append("<li><span class='contact'>" +  newContact.fullName() + "</span></li>");
 
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
+
+    $(".contact").last().click(function() {
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.firstName);
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+    });
   });
-});
-$(".contact").last().click(function() {
-  $("#show-contact").show();
-  $("#show-contact h2").text(newContact.firstName);
-  $(".first-name").text(newContact.firstName);
-  $(".last-name").text(newContact.lastName);
 });
